1346ae1c00f6418216c36442a66efe98af366e47
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-08-01 07:30:03 -0300

    I made some updates in the Collection extension.

--- Sources/App/Extensions/Collection+Extension.swift
+++ Sources/App/Extensions/Collection+Extension.swift
@@ -5,9 +5,12 @@
 //  Created by Isaque da Silva on 31/07/24.
 //
 
-import Fluent
+import Foundation
 
-extension Collection where Element: DatabaseOperation {
+extension Collection where Element: DatabaseModel {
+    /// Transform any Collection of any model that is conforming
+    /// with the ``DatabaseModel`` protocol into a Read model.
+    /// - Returns: Returns an array of Read model.
     func readAll() throws -> [Element.Read] {
         try self.map { model in
             try model.read()
