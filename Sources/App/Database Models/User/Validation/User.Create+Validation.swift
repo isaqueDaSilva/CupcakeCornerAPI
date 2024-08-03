f8979a9982673fa2a46eccd73c2fce07cb52a432
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-08-03 04:15:20 -0300

    I added the validation process to User's Create DTO.

--- Sources/App/Database Models/User/Validation/User.Create+Validation.swift
+++ Sources/App/Database Models/User/Validation/User.Create+Validation.swift
@@ -5,4 +5,29 @@
 //  Created by Isaque da Silva on 03/08/24.
 //
 
-import Foundation
+import Vapor
+
+extension User.Create: Validatable {
+    static func validations(_ validations: inout Validations) {
+        validations.add(
+            User.FieldName.name.validationKey,
+            as: String.self,
+            is: !.empty,
+            required: true
+        )
+        
+        validations.add(
+            User.FieldName.email.validationKey,
+            as: String.self,
+            is: !.empty && .email,
+            required: true
+        )
+        
+        validations.add(
+            User.FieldName.password.validationKey,
+            as: String.self,
+            is: .count(8...),
+            required: true
+        )
+    }
+}
