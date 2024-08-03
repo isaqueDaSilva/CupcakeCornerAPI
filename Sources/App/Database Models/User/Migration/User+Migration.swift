f61afcedaf76bc89931fc59d96561243d2cd6ad0
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-08-03 04:16:23 -0300

    I created the User's Migration plan.

--- Sources/App/Database Models/User/Migration/User+Migration.swift
+++ Sources/App/Database Models/User/Migration/User+Migration.swift
@@ -5,4 +5,33 @@
 //  Created by Isaque da Silva on 02/08/24.
 //
 
-import Foundation
+import Fluent
+
+extension User {
+    struct Migration: AsyncMigration {
+        let userSchemaName = SchemaName.user.rawValue
+        let roleSchemaName = SchemaName.role.rawValue
+        let paymentMethodSchemaName = SchemaName.paymentMethod.rawValue
+        
+        func prepare(on database: any Database) async throws {
+            
+            let role = try await database.enum(roleSchemaName).read()
+            let paymentMethod = try await database.enum(paymentMethodSchemaName).read()
+            
+            try await database.schema(userSchemaName)
+                .id()
+                .field(FieldName.name.key, .string, .required)
+                .field(FieldName.email.key, .string, .required)
+                .field(FieldName.passwordHash.key, .string, .required)
+                .field(FieldName.role.key, role, .required)
+                .field(FieldName.paymentMethod.key, paymentMethod, .required)
+                .unique(on: FieldName.email.key)
+                .create()
+        }
+        
+        func revert(on database: any Database) async throws {
+            try await database.schema(userSchemaName)
+                .delete()
+        }
+    }
+}
