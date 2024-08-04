43176ee4129653dd81c784ec87ac98753011fe41
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-08-03 23:20:52 -0300

    I created the EnsureAdminUserMiddleware. This middleware will check if the user logged has the admin role before to execute some action.

--- Sources/App/Middleware/EnsureAdminUserMiddleware.swift
+++ Sources/App/Middleware/EnsureAdminUserMiddleware.swift
@@ -5,4 +5,14 @@
 //  Created by Isaque da Silva on 03/08/24.
 //
 
-import Foundation
+import Vapor
+
+struct EnsureAdminUserMiddleware: AsyncMiddleware {
+    func respond(to request: Request, chainingTo next: any AsyncResponder) async throws -> Response {
+        guard let user = request.auth.get(User.self), user.role == .admin else {
+            throw Abort(.unauthorized)
+        }
+        
+        return try await next.respond(to: request)
+    }
+}
