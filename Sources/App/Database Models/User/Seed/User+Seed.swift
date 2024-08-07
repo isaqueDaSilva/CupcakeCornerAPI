0d14f451e6d01536f9bce6bc0ed914586f06bf29
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-08-07 09:02:47 -0300

    I created the user seed This seed is the default user admin for Cupcake corner.

---
+++ Sources/App/Database Models/User/Seed/User+Seed.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Isaque da Silva on 8/7/24.
+//
+
+import Fluent
+import Vapor
+
+extension User {
+    struct Seed: AsyncMigration {
+        let adminName = Environment.get("ADMIN_NAME")
+        let adminEmail = Environment.get("ADMIN_EMAIL")
+        let adminPassword = Environment.get("ADMIN_PASSWORD")
+        
+        func prepare(on database: Database) async throws {
+            guard let adminName, let adminEmail, let adminPassword else {
+                throw Abort(.notAcceptable)
+            }
+            
+            let user = Create(
+                name: adminName,
+                email: adminEmail,
+                password: adminPassword,
+                role: .admin,
+                paymentMethod: .isAdmin
+            )
+            
+            let passwordHash = try Bcrypt.hash(adminPassword)
+            
+            let admin = User(from: user, and: passwordHash)
+            
+            try await admin.create(on: database)
+        }
+        
+        func revert(on database: Database) async throws {
+            try await User.query(on: database)
+                .delete()
+        }
+    }
+}
